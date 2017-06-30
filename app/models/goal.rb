84217d67ccc93e2d1b3a3fa16e54a0982ac7dddd
afuerstenau <alex@fuerstenau-clan.de>
2017-06-30 22:05:51 +0200

    highlight recently changes goals

--- app/models/goal.rb
+++ app/models/goal.rb
@@ -1,2 +1,9 @@
 class Goal < ApplicationRecord
+  def has_changed_today()
+    year_month_day = "%Y-%m-%d"
+    logger.error "has_changed_today? #{updated_at.strftime(year_month_day) == Date.today.strftime(year_month_day)}"
+    logger.error "updated: #{updated_at.strftime(year_month_day)}"
+    logger.error "today: #{Date.today.strftime(year_month_day)}"
+    return updated_at.strftime(year_month_day) == Date.today.strftime(year_month_day)
+  end
 end
