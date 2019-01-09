fe9b8b668ca7d57b24df8d0312785f1de1858e30
Alex Fürstenau <alex@familie-fuerstenau.de>
2019-01-09 16:32:35 +0100

    refactored

--- app/models/goal.rb
+++ app/models/goal.rb
@@ -1,10 +1,8 @@
 class Goal < ApplicationRecord
   has_many :notes
-  def has_changed_today()
-    year_month_day = "%Y-%m-%d"
-    logger.error "has_changed_today? #{updated_at.strftime(year_month_day) == Date.today.strftime(year_month_day)}"
-    logger.error "updated: #{updated_at.strftime(year_month_day)}"
-    logger.error "today: #{Date.today.strftime(year_month_day)}"
-    return updated_at.strftime(year_month_day) == Date.today.strftime(year_month_day)
+
+  def changed_today?
+    year_month_day = '%Y-%m-%d'
+    updated_at.strftime(year_month_day) == Date.today.strftime(year_month_day)
   end
 end
